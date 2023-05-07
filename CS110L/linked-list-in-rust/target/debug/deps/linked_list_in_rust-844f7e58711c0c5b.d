9098e57c3ea9e2d4644dc52d401a892676fc5e60
UnicoCN <ivanhugh524@outlook.com>
2023-05-07 16:52:05 +0800

    remove target and add .gitignore

--- CS110L/linked-list-in-rust/target/debug/deps/linked_list_in_rust-844f7e58711c0c5b.d
+++
@@ -1,5 +0,0 @@
-/Users/ivan/Code/learn-rust/learn-rust/CS110L/linked-list-in-rust/target/debug/deps/linked_list_in_rust-844f7e58711c0c5b.rmeta: src/main.rs
-
-/Users/ivan/Code/learn-rust/learn-rust/CS110L/linked-list-in-rust/target/debug/deps/linked_list_in_rust-844f7e58711c0c5b.d: src/main.rs
-
-src/main.rs:
